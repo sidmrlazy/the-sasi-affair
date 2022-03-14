fd5879cda1eabcde8c62a719dea92afcbd244f0f
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-03-14 14:40:50 +0530

    Updated

--- home/couple.php
+++ home/couple.php
@@ -13,7 +13,7 @@
 </div>
 
 
-<!-- Siddharth Image Modal -->
+<!-- Siddharth Image Modal Section -->
 <div class="modal fade" id="siddharthModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog modal-dialog-centered modal-xl">
         <div class="modal-content">
