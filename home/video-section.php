7ddeeddf1e32846b22168b4af86d2279365d908c
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-04-04 17:42:07 +0530

    Updated

--- home/video-section.php
+++ home/video-section.php
@@ -33,7 +33,7 @@
                     <div class="video-row">
                         <div class="card col-md-5" style="width: 18rem;">
                             <video class="video" controls muted autoplay>
-                                <source src="videos/video1.mp4" type="video/mp4">
+                                <source src="videos/video4.mp4" type="video/mp4">
                                 <source src="movie.ogg" type="video/ogg">
                                 Your browser does not support the video tag.
                             </video>
@@ -41,7 +41,7 @@
 
                         <div class="card col-md-5" style="width: 18rem;">
                             <video class="video" controls muted autoplay>
-                                <source src="videos/video2.mp4" type="video/mp4">
+                                <source src="videos/video5.mp4" type="video/mp4">
                                 <source src="movie.ogg" type="video/ogg">
                                 Your browser does not support the video tag.
                             </video>
@@ -49,7 +49,7 @@
 
                         <div class="card col-md-5" style="width: 18rem;">
                             <video class="video" controls muted autoplay>
-                                <source src="videos/video3.mp4" type="video/mp4">
+                                <source src="videos/video6.mp4" type="video/mp4">
                                 <source src="movie.ogg" type="video/ogg">
                                 Your browser does not support the video tag.
                             </video>
