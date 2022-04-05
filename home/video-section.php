f381e8e4f966fb1520f41e475933b8ecc7ac4ce4
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-04-05 15:16:25 +0530

    Updated

--- home/video-section.php
+++ home/video-section.php
@@ -129,14 +129,6 @@
                                 Your browser does not support the video tag.
                             </video>
                         </div>
-
-                        <!-- <div class="card col-md-5" style="width: 18rem;">
-                            <video class="video" controls muted autoplay>
-                                <source src="videos/video15.mp4" type="video/mp4">
-                                <source src="movie.ogg" type="video/ogg">
-                                Your browser does not support the video tag.
-                            </video>
-                        </div> -->
                     </div>
                 </div>
 
