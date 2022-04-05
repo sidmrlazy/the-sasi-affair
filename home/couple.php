03c49bcda95c9410dfbecf5b1da638ddb6d93edd
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2022-04-05 14:08:06 +0530

    ok

--- home/couple.php
+++ home/couple.php
@@ -7,25 +7,25 @@
                 <div id="carouselExampleControlsbride" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="images/cover/bride/bride.jpg" class="d-block w-100" alt="">
+                            <img src="images/cover/bride/bride.jpg" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/bride/bride2.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/bride/bride2.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/bride/bride3.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/bride/bride3.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/bride/bride4.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/bride/bride4.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/bride/bride5.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/bride/bride5.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/bride/bride6.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/bride/bride6.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/bride/bride7.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/bride/bride7.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsbride"
@@ -55,19 +55,19 @@
                 <div id="carouselExampleControlsGroom" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img src="images/cover/groom/groom.jpg" class="d-block w-100" alt="">
+                            <img src="images/cover/groom/groom.jpg" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/groom/groom2.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/groom/groom2.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/groom/groom3.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/groom/groom3.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/groom/groom4.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/groom/groom4.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                         <div class="carousel-item">
-                            <img src="images/cover/groom/groom5.webp" class="d-block w-100" alt="">
+                            <img src="images/cover/groom/groom5.webp" class="d-block img-fluid w-100" alt="">
                         </div>
                     </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsGroom"
@@ -91,13 +91,13 @@
         <div class="row">
             <div class="col-md-6 couple" data-bs-toggle="modal" data-bs-target="#samikshaModal">
                 <div class="couple-content">
-                    <img src="images/cover/bride/bride.jpg" alt="">
+                    <img src="images/cover/bride/bride.jpg" alt="" class="img-fluid">
                     <h1>Samiksha (Sa)</h1>
                 </div>
             </div>
             <div class="col-md-6 couple" data-bs-toggle="modal" data-bs-target="#siddharthModal">
                 <div class="couple-content">
-                    <img src="images/cover/groom/groom.jpg" alt="">
+                    <img src="images/cover/groom/groom.jpg" alt="" class="img-fluid">
                     <h1>Siddharth (Si)</h1>
                 </div>
             </div>
